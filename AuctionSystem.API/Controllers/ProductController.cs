3098369d00e43cf442a1c1422d0d43aaf0cb3482
MD. Shahid Hasan <shahidhasanshourav@gmail.com>
2026-04-13 02:37:13 +0600

    Imp: product controller

---
+++ AuctionSystem.API/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+using AuctionSystem.Core.Interfaces.Apps;
+using AuctionSystem.Core.Models;
+using AuctionSystem.Core.Models.Product;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionSystem.API.Controllers;
+
+[ApiController]
+[Route("products")]
+public class ProductController : ApiBaseController
+{
+    private readonly ILogger<ProductController> _logger;
+    private readonly IProductApp _productApp;
+
+    public ProductController(ILogger<ProductController> logger, IProductApp productApp)
+    {
+        _logger = logger;
+        _productApp = productApp;
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<ServiceResult<List<ProductReadModel>>>> GetProducts()
+    {
+        _logger.LogDebug("GetProducts()");
+
+        var result = await _productApp.GetProducts();
+
+        return new JsonResult(result) { StatusCode = result.Code };
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ServiceResult<ProductReadModel>>> GetProductById(int id)
+    {
+        _logger.LogDebug("GetProductById() {id}", id);
+
+        var result = await _productApp.GetProductById(id);
+
+        return new JsonResult(result) { StatusCode = result.Code };
+    }
+}
